cf51c27292e2bd8b0279ac59d739b5304b5658c0
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-06-20 12:15:24 +0200

    adding contact method into controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,24 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def contact
+    @sender_email = params[:sender_email]
+    @sender_first_name = params[:sender_email]
+    @sender_last_name = params[:sender_email]
+    @sender_phone = params[:sender_phone]
+    @sender_place_of_intervention = params[:sender_place_of_intervention]
+    @sender_demand = params[:sender_demand]
+    @sender_message = params[:sender_message]
+
+    @receiver_email = "contact@jardica.net"
+
+    if ContactMailer.with(sender_email: @sender_email, sender_first_name: @sender_first_name, sender_last_name: @sender_last_name, sender_phone: @sender_phone, sender_place_of_intervention: @sender_place_of_intervention, sender_demand: @sender_demand, sender_message: @sender_message).contact_email.deliver_now
+      flash[:success] = "Message sent!"
+      redirect_to root_path
+    else
+      flash[:danger] = "Message not sent!"
+      redirect_to root_path
+    end
+  end
 end
