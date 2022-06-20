7c5d60be77fbf7ea23f428f6c1686f43a4f273d5
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-06-20 16:02:52 +0200

    config mailer into pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :pages do
+    collection do
+      post :contact
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
