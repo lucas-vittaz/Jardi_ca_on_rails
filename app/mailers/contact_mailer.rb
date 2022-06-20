9c8ce0e69f3cbf8d070911a3d57413a6acca4db3
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-06-20 12:10:05 +0200

    adding mail template

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,2 +1,18 @@
 class ContactMailer < ApplicationMailer
+  default from: "contact@jardica.net"
+
+  def contact_email
+    @sender_email = params[:sender_email]
+    @sender_first_name = params[:sender_email]
+    @sender_last_name = params[:sender_email]
+    @sender_phone = params[:sender_phone]
+    @sender_place_of_intervention = params[:sender_place_of_intervention]
+    @sender_demand = params[:sender_demand]
+    @sender_message = params[:sender_message]
+
+    @receiver_email = params[:receiver_email]
+
+    mail(to: @receiver_email, subject: @sender_demand)
+  end
+
 end
